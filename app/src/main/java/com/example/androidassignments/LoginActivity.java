c3220a12645c6985c68baf19ce93c868fb8a4992
Torin99 <torinwylder@gmail.com>
2023-09-25 12:21:12 -0400

    Create Navigation System

--- app/src/main/java/com/example/androidassignments/LoginActivity.java
+++ app/src/main/java/com/example/androidassignments/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.androidassignments;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button logoutButton = super.findViewById(R.id.loginButton);
+        logoutButton.setOnClickListener((view) -> {
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
